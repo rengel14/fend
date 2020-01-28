e07876fc9f8f97e20f01356e83dfdb8719f0659f
Engel, Ryan (re153b) <re153b@att.com>
2020-01-28 09:06:40 -0600

    initial commit to get app base functionality

--- WeatherApp/server.js
+++ WeatherApp/server.js
@@ -1,19 +1,50 @@
-// Setup empty JS object to act as endpoint for all routes
+/* Empty JS object to act as endpoint for all routes */
 projectData = {};
 
-// Require Express to run server and routes
+/* Express to run server and routes */
+const express = require('express');
 
-// Start up an instance of app
+/* Start up an instance of app */
+const app = express();
 
+/* Dependencies */
+const bodyParser = require('body-parser')
 /* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+const cors = require('cors');
+app.use(cors());
 
-// Cors for cross origin allowance
-
-// Initialize the main project folder
+/* Initialize the main project folder*/
 app.use(express.static('website'));
 
+const port = 3000;
+/* Spin up the server*/
+const server = app.listen(port, listening);
+ function listening(){
+    // console.log(server);
+    console.log(`running on localhost: ${port}`);
+  };
 
-// Setup Server
+// GET route
+app.get('/all', sendData);
+
+function sendData (request, response) {
+  response.send(projectData);
+};
+
+// POST route
+app.post('/add', callBack);
+
+function callBack(req,res){
+  res.send('POST received');
+}
+
+// POST an animal
+const data = [];
+
+app.post('/animal', addAnimal);
+
+function addAnimal (req,res){
+    data.push(req.body);
+};
--- WeatherApp/website/app.js
+++ WeatherApp/website/app.js
@@ -3,3 +3,47 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+// Async POST
+const postData = async ( url = '', data = {})=>{
+    console.log(data);
+    const response = await fetch(url, {
+        method: 'POST', 
+        credentials: 'same-origin', 
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data), // body data type must match "Content-Type" header        
+    });
+
+    try {
+        const newData = await response.json();
+        return newData;
+    }catch(error) {
+        console.log("error", error);
+    }
+};
+
+// Async GET
+const retrieveData = async (url='') =>{ 
+    const request = await fetch(url);
+    try {
+        // Transform into JSON
+        const allData = await request.json()
+    }
+    catch(error) {
+        console.log("error", error);
+        // appropriately handle the error
+    }
+};
+
+// TODO-Chain your async functions to post an animal then GET the resulting data
+function postGet(){
+    postData('/animal', {fav:'lion'})
+      .then(function(data){
+        retrieveData('/all')
+    })
+}
+// TODO-Call the chained function
+postGet()
