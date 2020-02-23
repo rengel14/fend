d522f55fea9d69a29eefbc648e234c50dcdbe77c
Ryan Engel <rengel1414@gmail.com>
2020-02-23 16:54:18 -0600

    Travel app initial commit. Brings site up working with webpack

---
+++ src/client/index.js
@@ -0,0 +1,6 @@
+import { ButtonFunction } from './js/app'
+
+import './styles/style.scss'
+
+//binds the function to go on click
+document.getElementById('generate').addEventListener("click", ButtonFunction);
--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,13 +1,3 @@
-/* Global Variables */
-//stores the API key I was given upon account creation on the openweather website
-const apikey = 'c599d3eeab09a9a592da504c68a00bee';
-//base URL for the api
-const baseUrl = 'http://api.openweathermap.org/data/2.5/forecast?';
-
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = (d.getMonth()+1) + '.' + d.getDate() + '.' + d.getFullYear();
-
 // Async POST
 const postData = async ( url = '', data = {})=>{
     const response = await fetch(url, {
@@ -17,73 +7,81 @@ const postData = async ( url = '', data = {})=>{
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(data), // body data type must match "Content-Type" header        
-    });
+    })
     try {
-        await response.json();
+        await response.json()
     }catch(error) {
-        console.log("error", error);
+        console.log("error", error)
     }
-};
+}
 
 // Async GET
 const retrieveData = async (url='') =>{ 
-    const request = await fetch(url);
+    const request = await fetch(url)
     try {
         // Transform into JSON
-        const data = await request.json();
+        const data = await request.json()
 
         //Update output html fields
-        document.getElementById('date').innerText = data[0].date;
-        document.getElementById('temp').innerText = data[0].temp;
-        document.getElementById('content').innerText = data[0].feelings;
+        document.getElementById('date').innerText = data[0].date
+        document.getElementById('temp').innerText = data[0].temp
+        document.getElementById('content').innerText = data[0].feelings
 
         //reset input html fields
-        document.getElementById('zip').value = "";
-        document.getElementById('feelings').value = "";
+        document.getElementById('zip').value = ""
+        document.getElementById('feelings').value = ""
     }
     catch(error) {
         // appropriately handle the error
-        console.log("error", error);
+        console.log("error", error)
     }
-};
+}
 
 const GetWeather = async () =>{ 
-    //gets zip code from html input field
-    const zip = document.getElementById('zip').value;
-    //constructs url to query
-    const url = baseUrl + 'APPID=' + apikey + '&zip=' + zip;
+    //stores the API key I was given upon account creation on the openweather website
+    const apikey = 'c599d3eeab09a9a592da504c68a00bee'
+    //base URL for the api
+    const baseUrl = 'http://api.openweathermap.org/data/2.5/forecast?'
 
-    const response = await fetch(url);
+    // Create a new date instance dynamically with JS
+    let d = new Date();
+    let newDate = (d.getMonth()+1) + '.' + d.getDate() + '.' + d.getFullYear();
+
+    //gets zip code from html input field
+    const zip = document.getElementById('zip').value
+    //constructs url to query
+    const url = baseUrl + 'APPID=' + apikey + '&zip=' + zip
+
+    const response = await fetch(url)
     try {
         // Transform into JSON
-        const data = await response.json();
+        const data = await response.json()
         
         //Construct Journal Entry object
-        object = {
+        const object = {
             //converts Kelvin temperature to Fahrenheit 
             'temp': Math.round((((data.list[0].main.temp) - 273.15) * (9/5)) + 32),
             'date': newDate,
             'feelings': document.getElementById('feelings').value
         }
         //returns a description of current weather conditions at time of posting, the date, and the recorded feelings
-        return (object);
+        return (object)
     }
     catch(error) {
         // appropriately handle the error
-        console.log("error", error);
+        console.log("error", error)
     }
-};
+}
 
 //constructs a function to run when the button is clicked
 const ButtonFunction = function() {
     GetWeather()
         .then(function(data) {
-            postData('/', data);
+            postData('http://localhost:3000/', data)
         })
         .then(function(data) {
-            retrieveData('/get');
-        });
-};
+            retrieveData('http://localhost:3000/get')
+        })
+}
 
-//binds the function to go on click
-document.getElementById('generate').addEventListener("click", ButtonFunction);
+export { ButtonFunction }
